af66eb28385471ee4350598046468d45cd1081aa
ChengTao <chengtaolearn@163.com>
2017-11-03 11:15:19 +0800

    refactor: 规范代码

--- library/src/main/java/com/chengtao/pianoview/view/PianoView.java
+++ library/src/main/java/com/chengtao/pianoview/view/PianoView.java
@@ -104,6 +104,36 @@ public class PianoView extends View {
     square = new RectF();
   }
 
+  @Override protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+    Log.e(TAG, "onMeasure");
+    Drawable whiteKeyDrawable = ContextCompat.getDrawable(context, R.drawable.white_piano_key);
+    //最小高度
+    int whiteKeyHeight = whiteKeyDrawable.getIntrinsicHeight();
+    //获取布局中的高度和宽度及其模式
+    int width = MeasureSpec.getSize(widthMeasureSpec);
+    int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+    int height = MeasureSpec.getSize(heightMeasureSpec);
+    //设置高度
+    switch (heightMode) {
+      case MeasureSpec.EXACTLY:
+        Log.e(TAG, "EXACTLY");
+        break;
+      case MeasureSpec.AT_MOST:
+        Log.e(TAG, "AT_MOST");
+        height = Math.min(height, whiteKeyHeight);
+        break;
+      case MeasureSpec.UNSPECIFIED:
+        Log.e(TAG, "UNSPECIFIED");
+        height = whiteKeyHeight;
+        break;
+    }
+    //设置缩放比例
+    scale = (float) (height - getPaddingTop() - getPaddingBottom()) / (float) (whiteKeyHeight);
+    layoutWidth = width - getPaddingLeft() - getPaddingRight();
+    //设置布局高度和宽度
+    setMeasuredDimension(width, height);
+  }
+
   @Override protected void onDraw(Canvas canvas) {
     Log.e(TAG, "onDraw");
     //初始化钢琴
@@ -166,36 +196,6 @@ public class PianoView extends View {
     }
   }
 
-  @Override protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-    Log.e(TAG, "onMeasure");
-    Drawable whiteKeyDrawable = ContextCompat.getDrawable(context, R.drawable.white_piano_key);
-    //最小高度
-    int whiteKeyHeight = whiteKeyDrawable.getIntrinsicHeight();
-    //获取布局中的高度和宽度及其模式
-    int width = MeasureSpec.getSize(widthMeasureSpec);
-    int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-    int height = MeasureSpec.getSize(heightMeasureSpec);
-    //设置高度
-    switch (heightMode) {
-      case MeasureSpec.EXACTLY:
-        Log.e(TAG, "EXACTLY");
-        break;
-      case MeasureSpec.AT_MOST:
-        Log.e(TAG, "AT_MOST");
-        height = Math.min(height, whiteKeyHeight);
-        break;
-      case MeasureSpec.UNSPECIFIED:
-        Log.e(TAG, "UNSPECIFIED");
-        height = whiteKeyHeight;
-        break;
-    }
-    //设置缩放比例
-    scale = (float) (height - getPaddingTop() - getPaddingBottom()) / (float) (whiteKeyHeight);
-    layoutWidth = width - getPaddingLeft() - getPaddingRight();
-    //设置布局高度和宽度
-    setMeasuredDimension(width, height);
-  }
-
   @Override public boolean onTouchEvent(MotionEvent event) {
     if (canPress) {
       switch (event.getAction()) {
@@ -238,60 +238,6 @@ public class PianoView extends View {
   }
 
   /**
-   * 处理多点触控时，手指抬起事件
-   *
-   * @param pointerId 触摸点ID
-   */
-  private void handlePointerUp(int pointerId) {
-    for (PianoKey key : pressedKeys) {
-      if (key.getFingerID() == pointerId) {
-        key.setPressed(false);
-        key.resetFingerID();
-        key.getKeyDrawable().setState(new int[] { -android.R.attr.state_pressed });
-        invalidate(key.getKeyDrawable().getBounds());
-        pressedKeys.remove(key);
-        break;
-      }
-    }
-  }
-
-  /**
-   * 处理滑动
-   *
-   * @param which 触摸点下标
-   * @param event 事件对象
-   */
-  private void handleMove(int which, MotionEvent event) {
-    int x = (int) event.getX(which) + this.getScrollX();
-    int y = (int) event.getY(which);
-    for (PianoKey key : pressedKeys) {
-      if (key.getFingerID() == event.getPointerId(which)) {
-        if (!key.contains(x, y)) {
-          key.getKeyDrawable().setState(new int[] { -android.R.attr.state_pressed });
-          invalidate(key.getKeyDrawable().getBounds());
-          key.setPressed(false);
-          key.resetFingerID();
-          pressedKeys.remove(key);
-        }
-      }
-    }
-  }
-
-  /**
-   * 处理最后一个手指抬起事件
-   */
-  private void handleUp() {
-    if (pressedKeys.size() > 0) {
-      for (PianoKey key : pressedKeys) {
-        key.getKeyDrawable().setState(new int[] { -android.R.attr.state_pressed });
-        key.setPressed(false);
-        invalidate(key.getKeyDrawable().getBounds());
-      }
-      pressedKeys.clear();
-    }
-  }
-
-  /**
    * 处理按下事件
    *
    * @param which 那个触摸点
@@ -319,6 +265,28 @@ public class PianoView extends View {
   }
 
   /**
+   * 处理白键点击
+   *
+   * @param which 那个触摸点
+   * @param event 事件
+   * @param key 钢琴按键
+   */
+  private void handleWhiteKeyDown(int which, MotionEvent event, PianoKey key) {
+    key.getKeyDrawable().setState(new int[] { android.R.attr.state_pressed });
+    key.setPressed(true);
+    if (event != null) {
+      key.setFingerID(event.getPointerId(which));
+    }
+    pressedKeys.add(key);
+    invalidate(key.getKeyDrawable().getBounds());
+    utils.playMusic(Piano.PianoKeyType.WHITE, key.getGroup(), key.getPositionOfGroup());
+    if (pianoListener != null) {
+      pianoListener.onPianoClick(key.getType(), key.getVoice(), key.getGroup(),
+          key.getPositionOfGroup());
+    }
+  }
+
+  /**
    * 处理黑键点击
    *
    * @param which 那个触摸点
@@ -341,26 +309,60 @@ public class PianoView extends View {
   }
 
   /**
-   * 处理白键点击
+   * 处理滑动
    *
-   * @param which 那个触摸点
-   * @param event 事件
-   * @param key 钢琴按键
+   * @param which 触摸点下标
+   * @param event 事件对象
    */
-  private void handleWhiteKeyDown(int which, MotionEvent event, PianoKey key) {
-    key.getKeyDrawable().setState(new int[] { android.R.attr.state_pressed });
-    key.setPressed(true);
-    if (event != null) {
-      key.setFingerID(event.getPointerId(which));
-    }
-    pressedKeys.add(key);
-    invalidate(key.getKeyDrawable().getBounds());
-    utils.playMusic(Piano.PianoKeyType.WHITE, key.getGroup(), key.getPositionOfGroup());
-    if (pianoListener != null) {
-      pianoListener.onPianoClick(key.getType(), key.getVoice(), key.getGroup(),
-          key.getPositionOfGroup());
+  private void handleMove(int which, MotionEvent event) {
+    int x = (int) event.getX(which) + this.getScrollX();
+    int y = (int) event.getY(which);
+    for (PianoKey key : pressedKeys) {
+      if (key.getFingerID() == event.getPointerId(which)) {
+        if (!key.contains(x, y)) {
+          key.getKeyDrawable().setState(new int[] { -android.R.attr.state_pressed });
+          invalidate(key.getKeyDrawable().getBounds());
+          key.setPressed(false);
+          key.resetFingerID();
+          pressedKeys.remove(key);
+        }
+      }
     }
   }
+
+  /**
+   * 处理多点触控时，手指抬起事件
+   *
+   * @param pointerId 触摸点ID
+   */
+  private void handlePointerUp(int pointerId) {
+    for (PianoKey key : pressedKeys) {
+      if (key.getFingerID() == pointerId) {
+        key.setPressed(false);
+        key.resetFingerID();
+        key.getKeyDrawable().setState(new int[] { -android.R.attr.state_pressed });
+        invalidate(key.getKeyDrawable().getBounds());
+        pressedKeys.remove(key);
+        break;
+      }
+    }
+  }
+
+  /**
+   * 处理最后一个手指抬起事件
+   */
+  private void handleUp() {
+    if (pressedKeys.size() > 0) {
+      for (PianoKey key : pressedKeys) {
+        key.getKeyDrawable().setState(new int[] { -android.R.attr.state_pressed });
+        key.setPressed(false);
+        invalidate(key.getKeyDrawable().getBounds());
+      }
+      pressedKeys.clear();
+    }
+  }
+
+  //-----公共方法
 
   /**
    * 自动播放
@@ -442,6 +444,106 @@ public class PianoView extends View {
     }.start();
   }
 
+  public int getPianoWidth() {
+    if (piano != null) {
+      return piano.getPianoWith();
+    }
+    return 0;
+  }
+
+  /**
+   * 获取钢琴布局的实际宽度
+   *
+   * @return 钢琴布局的实际宽度
+   */
+  public int getLayoutWidth() {
+    return layoutWidth;
+  }
+
+  /**
+   * 设置显示音名的矩形的颜色<br>
+   * <b>注:一共9中颜色</b>
+   *
+   * @param pianoColors 颜色数组，长度为9
+   */
+  public void setPianoColors(String[] pianoColors) {
+    if (pianoColors.length == 9) {
+      this.pianoColors = pianoColors;
+    }
+  }
+
+  /**
+   * 设置是否可点击
+   *
+   * @param canPress 是否可点击
+   */
+  public void setCanPress(boolean canPress) {
+    this.canPress = canPress;
+  }
+
+  /**
+   * 移动
+   *
+   * @param progress 移动百分比
+   */
+  public void scroll(int progress) {
+    switch (progress) {
+      case 0:
+        this.scrollTo(0, 0);
+        break;
+      case 100:
+        this.scrollTo(getPianoWidth() - getLayoutWidth(), 0);
+        break;
+      default:
+        this.scrollTo(
+            (int) (((float) progress / 100f) * (float) (getPianoWidth() - getLayoutWidth())), 0);
+        break;
+    }
+    this.progress = progress;
+  }
+
+  //接口
+
+  /**
+   * 初始化钢琴相关界面
+   *
+   * @param pianoListener 钢琴接口
+   */
+  public void setPianoListener(OnPianoListener pianoListener) {
+    this.pianoListener = pianoListener;
+  }
+
+  /**
+   * 设置加载音频接口
+   *
+   * @param loadAudioListener 　音频接口
+   */
+  public void setLoadAudioListener(OnLoadAudioListener loadAudioListener) {
+    this.loadAudioListener = loadAudioListener;
+  }
+
+  /**
+   * 设置自动播放接口
+   *
+   * @param autoPlayListener 　自动播放接口
+   */
+  public void setAutoPlayListener(OnPianoAutoPlayListener autoPlayListener) {
+    this.autoPlayListener = autoPlayListener;
+  }
+
+  //-----私有方法
+
+  /**
+   * 将dp装换成px
+   *
+   * @param dp dp值
+   * @return px值
+   */
+  private int dpToPx(int dp) {
+    DisplayMetrics displayMetrics = getContext().getResources().getDisplayMetrics();
+    return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+  }
+
   /**
    * 处理自动播放
    *
@@ -485,102 +587,6 @@ public class PianoView extends View {
         }
         break;
     }
-  }
-
-  /**
-   * 初始化钢琴相关界面
-   *
-   * @param pianoListener 钢琴接口
-   */
-  public void setPianoListener(OnPianoListener pianoListener) {
-    this.pianoListener = pianoListener;
-  }
-
-  /**
-   * 设置加载音频接口
-   *
-   * @param loadAudioListener 　音频接口
-   */
-  public void setLoadAudioListener(OnLoadAudioListener loadAudioListener) {
-    this.loadAudioListener = loadAudioListener;
-  }
-
-  /**
-   * 设置自动播放接口
-   *
-   * @param autoPlayListener 　自动播放接口
-   */
-  public void setAutoPlayListener(OnPianoAutoPlayListener autoPlayListener) {
-    this.autoPlayListener = autoPlayListener;
-  }
-
-  public int getPianoWidth() {
-    if (piano != null) {
-      return piano.getPianoWith();
-    }
-    return 0;
-  }
-
-  /**
-   * 移动
-   *
-   * @param progress 移动百分比
-   */
-  public void scroll(int progress) {
-    switch (progress) {
-      case 0:
-        this.scrollTo(0, 0);
-        break;
-      case 100:
-        this.scrollTo(getPianoWidth() - getLayoutWidth(), 0);
-        break;
-      default:
-        this.scrollTo(
-            (int) (((float) progress / 100f) * (float) (getPianoWidth() - getLayoutWidth())), 0);
-        break;
-    }
-    this.progress = progress;
-  }
-
-  /**
-   * 将dp装换成px
-   *
-   * @param dp dp值
-   * @return px值
-   */
-  private int dpToPx(int dp) {
-    DisplayMetrics displayMetrics = getContext().getResources().getDisplayMetrics();
-    return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
-  }
-
-  /**
-   * 获取钢琴布局的实际宽度
-   *
-   * @return 钢琴布局的实际宽度
-   */
-  public int getLayoutWidth() {
-    return layoutWidth;
-  }
-
-  /**
-   * 设置显示音名的矩形的颜色<br>
-   * <b>注:一共9中颜色</b>
-   *
-   * @param pianoColors 颜色数组，长度为9
-   */
-  public void setPianoColors(String[] pianoColors) {
-    if (pianoColors.length == 9) {
-      this.pianoColors = pianoColors;
-    }
-  }
-
-  /**
-   * 设置是否可点击
-   *
-   * @param canPress 是否可点击
-   */
-  public void setCanPress(boolean canPress) {
-    this.canPress = canPress;
   }
 
   @Override protected Parcelable onSaveInstanceState() {
