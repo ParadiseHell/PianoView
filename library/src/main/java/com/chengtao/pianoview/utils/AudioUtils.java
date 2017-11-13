516f93c792f067154ee4c46a3b2aec778f2823ad
ChengTao <chengtaolearn@163.com>
2017-11-13 14:33:38 +0800

    refactor: 更新音频文件(使用ogg)

--- library/src/main/java/com/chengtao/pianoview/utils/AudioUtils.java
+++ library/src/main/java/com/chengtao/pianoview/utils/AudioUtils.java
@@ -28,7 +28,7 @@ public class AudioUtils implements LoadAudioMessage {
   //线程池,用于加载和播放音频
   private ExecutorService service = Executors.newCachedThreadPool();
   //最大音频数目
-  private final static int MAX_STREAM = 4;
+  private final static int MAX_STREAM = 5;
   private static AudioUtils instance = null;
   //消息ID
   private final static int LOAD_START = 1;
